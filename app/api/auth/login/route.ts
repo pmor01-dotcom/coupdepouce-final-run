f63694dde3bc32a9f0cbdb479b2c71d892b43024
Paul <pmor01@free.fr>
2026-06-07 16:19:47 +0200

    push again

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../lib/prisma';
-import { supabase } from '../../../../lib/supabase';
+import { supabase } from '../../../../lib/supabase-server';
 
 export async function POST(request: NextRequest) {
   try {
