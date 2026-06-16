ce455d7885bca7fbcec39bd5e3e7a5ad7833807c
Paul <pmor01@free.fr>
2026-06-16 22:49:08 +0200

    push lang prov chng

--- app/messages/[id]/page.tsx
+++ app/messages/[id]/page.tsx
@@ -4,7 +4,8 @@ import { useEffect, useState, useRef } from 'react'
 import { useParams } from 'next/navigation'
 import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
 import AuthProvider, { useAuth } from '@/app/components/AuthProvider'
-import LanguageProvider, { useLanguage } from '@/app/components/LanguageProvider'
+import { LanguageProvider, useLanguage } from '@/app/components/LanguageProvider'
+
 
 
 export default function ConversationPage() {
