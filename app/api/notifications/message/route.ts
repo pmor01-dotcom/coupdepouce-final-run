59aa418a83a15cea07800ea1d978f8744bc8f8a5
Paul <pmor01@free.fr>
2026-06-23 10:45:54 +0200

    push

--- app/api/notifications/message/route.ts
+++ app/api/notifications/message/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '../../../../lib/prisma'
+
 import EmailService from '../../../../lib/email'
 
 export async function POST(request: NextRequest) {
