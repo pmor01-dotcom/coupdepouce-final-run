84e254b0a5c7e317041c895157b40eb0715c5241
Paul <pmor01@free.fr>
2026-06-23 11:59:29 +0200

    push

--- lib/email.ts
+++ lib/email.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
-import EmailService from "../../../../lib/email";
+import EmailService from '../../../lib/email'
+
 
 export async function POST(request: NextRequest) {
   try {
