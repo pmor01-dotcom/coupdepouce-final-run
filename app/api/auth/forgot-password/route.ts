5cf74c7643f5c07ad8f300016dfe94a7e4a658be
Paul <pmor01@free.fr>
2026-06-23 10:53:50 +0200

    pus

--- app/api/auth/forgot-password/route.ts
+++ app/api/auth/forgot-password/route.ts
@@ -1,5 +1,4 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { prisma } from '../../../../lib/prisma'
 import crypto from 'crypto'
 import EmailService from '../../../../lib/email'
 export const dynamic = 'force-dynamic'
