9b3e1785af9eabecbc199cedf6b018b5a54510c7
pmor01-dotcom <pmor01@free.fr>
2026-06-26 21:46:44 +0200

    Update route.ts

--- app/api/artisan/get-profile/route.ts
+++ app/api/artisan/get-profile/route.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export async function GET(request: NextRequest) {
   const supabase = createRouteHandlerClient({ cookies });
 
-  const {
+const {
     data: { session },
   } = await supabase.auth.getSession();
 
