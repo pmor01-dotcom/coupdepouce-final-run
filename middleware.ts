b2515920ad8b08ffb7e08247bd24d7564bb5c5e4
Paul <pmor01@free.fr>
2026-07-02 19:21:25 +0200

    push

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,11 @@ import { NextResponse } from 'next/server'
 export function middleware(req) {
   const url = req.nextUrl
 
+  // If already on /reset-password, do NOT redirect again
+  if (url.pathname === '/reset-password') {
+    return NextResponse.next()
+  }
+
   // If Supabase recovery code is present, redirect to reset-password page
   if (url.searchParams.has('code')) {
     url.pathname = '/reset-password'
